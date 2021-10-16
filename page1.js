3a6ffad867922d7d9b32439221ad268a602ddaf2
birman <birman126@gmail.com>
2021-10-16 13:46:24 +0400

    create page1

---
+++ page1.js
@@ -0,0 +1,5 @@
+console.log(1)
+
+const beline = () => {
+    document.location.href="file:///C:/Users/Birman/Desktop/%D0%A2%D0%B5%D1%81%D1%82%D0%BE%D0%B2%D0%B0%D1%8F%20%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B0/task1/Page2.html"
+}
