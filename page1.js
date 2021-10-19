006c10766bbcdec39947f46ba728865ac1db4835
birman <birman126@gmail.com>
2021-10-19 09:53:03 +0400

    add page2

--- page1.js
+++ page1.js
@@ -3,3 +3,13 @@ console.log(1)
 const beline = () => {
     document.location.href="file:///C:/Users/Birman/Desktop/%D0%A2%D0%B5%D1%81%D1%82%D0%BE%D0%B2%D0%B0%D1%8F%20%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B0/task1/Page2.html"
 }
+
+const isValid = () => {
+        var re = /^\d[\d\(\)\ -]{4,14}\d$/;
+        var myPhone = document.getElementById('phone').value;
+        var valid = re.test(myPhone);
+        if (valid) output = 'Номер телефона введен правильно!';
+        else output = 'Номер телефона введен неправильно!';
+        document.getElementById('message').innerHTML = document.getElementById('message').innerHTML+'<br />'+output;
+        
+}
