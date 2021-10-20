6b0cef8db227450c18892778fb631282adeb45f0
Birman <birman126@gmail.com>
2021-10-20 13:07:37 +0400

    add function

--- page1.js
+++ page1.js
@@ -1,9 +1,8 @@
 console.log(1)
-
-const beline = () => {
-    document.location.href="file:///C:/Users/Birman/Desktop/%D0%A2%D0%B5%D1%81%D1%82%D0%BE%D0%B2%D0%B0%D1%8F%20%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B0/task1/Page2.html"
-}
-
+var operator = '';
+//document.getElementById("operator").innerHTML = operator;
+console.log(operator)
+const operatorW = (operator) => {document.getElementById('id_operator').innerHTML=operator;}
 const isValid = () => {
         var re = /^\d[\d\(\)\ -]{4,14}\d$/;
         var myPhone = document.getElementById('phone').value;
