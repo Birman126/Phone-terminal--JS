f10a8bae2552e1f738757315775028e0d3fb2f9f
Birman <birman126@gmail.com>
2021-10-21 14:29:32 +0400

    add index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log(1)
-var operator ='s'
+var operator ='Оператор'
 var text = 'Би'
 
 const operatorW = (text) => {operator = text; console.log('функция - ', operator)}
@@ -12,5 +12,12 @@ const isValid = () => {
         if (valid) output = 'Номер телефона введен правильно!';
         else output = 'Номер телефона введен неправильно!';
         document.getElementById('message').innerHTML = document.getElementById('message').innerHTML+'<br />'+output;
-        
+}
+const isValidSum = () => {
+        var re = /^\d[1-1000]\d$/;
+        var myPhone = document.getElementById('Sum').value;
+        var valid = re.test(myPhone);
+        if (valid) output = 'Сумма введена правильно!';
+        else output = 'Введите сумму от 1 до 1000!';
+        document.getElementById('messageSum').innerHTML = document.getElementById('messageSum').innerHTML+'<br />'+output;
 }
