63452f2f1951bbae594aa672e69b7c3190760f80
Birman <birman126@gmail.com>
2021-10-22 12:45:09 +0400

    redact css

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 console.log(1)
-var operator ='Оператор'
-var text = 'Би'
-
+var operator ='Оператор';
+var text = 'Би';
+var sucess = true;
 const operatorW = (text) => {operator = text; console.log('функция - ', operator)}
 document.getElementById('id_operator').innerHTML=operator;
 console.log(operator)
@@ -11,13 +11,16 @@ const isValid = () => {
         var valid = re.test(myPhone);
         if (valid) output = 'Номер телефона введен правильно!';
         else output = 'Номер телефона введен неправильно!';
-        document.getElementById('message').innerHTML = document.getElementById('message').innerHTML+'<br />'+output;
+        document.getElementById('message').innerText = output;
 }
 const isValidSum = () => {
-        var re = /^\d[1-1000]\d$/;
-        var myPhone = document.getElementById('Sum').value;
-        var valid = re.test(myPhone);
-        if (valid) output = 'Сумма введена правильно!';
-        else output = 'Введите сумму от 1 до 1000!';
-        document.getElementById('messageSum').innerHTML = document.getElementById('messageSum').innerHTML+'<br />'+output;
+        var mySum = document.getElementById('Sum').value;
+        var valid = (mySum<=1000)&&(mySum>=1);
+        if (valid) sumOutput = 'Сумма введена правильно!';
+        else sumOutput = 'Введите сумму от 1 до 1000!';
+        document.getElementById('messageSum').innerHTML = sumOutput;
+}
+const isSucess = () => {
+        if (sucess) {return ("Пополнение счета " + myPhone + "на сумму" + mySum + "выполненно успешно")}
+        else {return ('Полнение счета не выполнено, попробуйте снова')}  
 }
